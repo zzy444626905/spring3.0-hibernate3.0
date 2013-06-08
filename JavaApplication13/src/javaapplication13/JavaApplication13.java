1ef4edfa8b0e466bc90e406abe0429bd994351be
zzy444626905 <zzy444626905@163.com>
2013-06-09 04:11:45 +0800

    Update JavaApplication13.java

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -17,6 +17,7 @@ public class JavaApplication13 {
     public static void main(String[] args) {
         // TODO code application logic heredddddddddddddddddddddddddddddddddddddddddddddfdfdfdfdfdfdf
         //hahahahaha
+        //test
         System.out.println("dddddddddddddddd");
         
     }
